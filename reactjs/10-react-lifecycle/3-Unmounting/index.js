8ede74109069a04c5fb290079c05688557fac12b
dat <dathalongbay@gmail.com>
2021-02-02 11:55:44 +0700

    222

---
+++ reactjs/10-react-lifecycle/3-Unmounting/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Container extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {show: true};
+    }
+    delHeader = () => {
+        this.setState({show: false});
+    }
+    render() {
+        let myheader;
+        if(this.state.show) {
+            myheader = <Child />
+        }
+        return (
+            <div>
+                {myheader}
+                <button type="button" onclick="{this.delHeader}">Delete Header</button>
+            </div>
+        );
+    }
+}
+
+class Child extends React.Component {
+    componentWillUnmount() {
+        alert("The component name header is about to the unmounted");
+    }
+    render() {
+        return (
+            <h1>Hello world</h1>
+        );
+    }
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'));
